4220061c63b10352db13d2c94de0e026e5678bf0
kimhg <kimhgedu@users.noreply.github.com>
2020-06-18 16:11:37 +0900

    [2020-06-18]initialize

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Carousel />
+    <CardList />
+  </div>
+</template>
+
+<script>
+import Carousel from '@/components/Carousel.vue'
+import CardList from '@/components/CardList.vue'
+export default {
+  name: 'Home',
+  components: {
+    Carousel,
+    CardList
+  }
+}
+</script>
